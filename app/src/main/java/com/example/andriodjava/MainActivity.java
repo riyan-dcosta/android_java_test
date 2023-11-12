102e0aae0ab095f7d01bc9f29d0a0e66a085ee60
Riyan <rdriyan@gmail.com>
2023-11-12 16:52:04 +0530

    xml textfield and button

--- app/src/main/java/com/example/andriodjava/MainActivity.java
+++ app/src/main/java/com/example/andriodjava/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.andriodjava;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_list);
+        Button btn = findViewById(R.id.add_product_button);
+        btn.setOnClickListener(view -> onButtonClick());
+    }
+
+    public void onButtonClick() {
+        TextInputEditText editText = (TextInputEditText) findViewById(R.id.product_id);
+        String productId = "N/A";
+        Editable val = editText.getText();
+        if (val != null && val.length() > 0) {
+            productId = val.toString();
+        }
+        String logMsg = String.format("Print the first input field value %s", productId);
+        Log.e("btnCLick", logMsg);
     }
 }
